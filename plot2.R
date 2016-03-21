d3f8ecac4087f9c2a0c04dcd7a6f0793bbd5b7fe
Felix Xinu <xinufelix@gmail.com>
2016-03-20 20:26:35 -0400

    Limit read between 2007-02-01 and 2007-02-02 inclusive

--- plot2.R
+++ plot2.R
@@ -1,32 +1,46 @@
-## Data are located under the 'data' subdirectory which is included with the repository.
-hpc <- read.csv("~/coursera/eda/ExData_Plotting1/data/household_power_consumption.txt",
-                                        sep=";", 
-                                        colClasses = c('character',
-                                                     'character',
-                                                     'numeric',
-                                                     'numeric',
-                                                     'numeric',
-                                                     'numeric',
-                                                     'numeric',
-                                                     'numeric',
-                                                     'numeric'),
-                                        na.strings="?",
-                                        stringsAsFactors=FALSE)
+## What is the line number of 2007-02-01 00:00:00?
+start_datetime <- grep("^1/2/2007", readLines('./data/household_power_consumption.txt')) - 1
 
-## Combine Date, Time columns into a datetime object.
-hpc$datetime <- as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
+## Adjust for presence of the header
+start_datetime <- start_datetime - 1
 
-## The plot starts on a Thursday, so get all of them and use the first one. 
-thursdays <- which(weekdays(hpc$datetime) == 'Thursday')
-
-## The range of interest is 48 hours which includes 2880 observations.
+## Number of minutes in two days
 two_days_min <- 2*24*60
 
-## Subset observations staring the first Thursday from the dataset as the starting point.
-datetime_range <- thursdays[1]:(thursdays[1]+two_days_min)
+## Start read from 2007-02-01 00:00:00 and include 2 days of observations.
+hpc <- read.csv("~/coursera/eda/ExData_Plotting1/data/household_power_consumption.txt",
+                header = TRUE,
+                skip=start_datetime,
+                nrow=two_days_min,
+                sep=";", 
+                colClasses = c('character',
+                               'character',
+                               'numeric',
+                               'numeric',
+                               'numeric',
+                               'numeric',
+                               'numeric',
+                               'numeric',
+                               'numeric'),
+                na.strings="?",
+                stringsAsFactors=FALSE)
+
+## Add the column names which were skipped during the file read.
+names(hpc) <- c('Date', 
+                'Time', 
+                'Global_active_power', 
+                'Global_reactive_power',
+                'Voltage', 
+                'Global_intensity', 
+                'Sub_metering_1', 
+                'Sub_metering_2', 
+                'Sub_metering_3')
+
+## Combine Date, Time columns into datetime
+hpc$datetime <- as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
 
 ## Plot of Global Active Power v. DateTime
-plot(hpc$datetime[datetime_range], hpc$Global_active_power[datetime_range],
+plot(hpc$datetime, hpc$Global_active_power,
      col='black',
      main='',
      ylab='Global Active Power (kilowatts)',
